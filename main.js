5a57ca42b28edba34952469490b480e6288916bb
Vityokart <vityokart@gmail.com>
2021-09-16 22:32:59 +0300

    Task 4

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-let money = +prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+let money, time;
+function start(){
+    money = +prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt("Ваш бюджет на месяц?");
+    }
+}
+start();
 
 let obj = {
     budget: money,
@@ -7,24 +15,74 @@ let obj = {
     expenses: {},
     optionalExpenses : {},
     income: [],
-    savings: false
+    savings: true
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", '') ,
-        b = prompt("Во сколько обойдется?", '');
-    if ((typeof(a)) === 'string' && typeof(a) != null && typeof(b) != null
-    && a != '' && b != '' && a.length < 50) {
-        console.log('done!');
-        obj.expenses[a] = b;
+function chooseExpenses () {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце") ,
+            b = prompt("Во сколько обойдется?");
+        if ((typeof(a)) === 'string' && typeof(a) != null && typeof(b) != null
+        && a != '' && b != '' && a.length < 50) {
+            console.log('done!');
+            obj.expenses[a] = b;
+        } else {
+            alert("Please, type correct info");
+            i--;
+        }   
+    }
+}
+
+chooseExpenses();
+
+function detectDayBudget () {
+    obj.moneyPerDay = (obj.budget / 30).toFixed();
+    alert("Budget per one day " + obj.moneyPerDay);
+}
+
+detectDayBudget();
+
+function chooseOptExpenses() {
+    for (let i = 0; i < 3; i++) {
+        let a = prompt('Введите статью необезательных разходов');
+        let b = prompt('Во сколько обойдется?');
+        if ((typeof(a)) === 'string' && typeof(a) != null && typeof(b) != null
+        && a != '' && b != '' && a.length < 50) {
+            console.log('great');
+            obj.optionalExpenses[a] = b;
+        } else {
+            alert("Please, type correct info");
+            i--;
+        }
+        
+    }
+}
+chooseOptExpenses();
+
+function detectLevel() {
+    if (obj.moneyPerDay < 100) {
+        console.log("Minimal lvl of grid"); 
+    } else if(obj.moneyPerDay > 100 && obj.moneyPerDay < 2000) {
+        console.log("Middle lvl of grid");
+    } else if (obj.moneyPerDay > 2000) {
+        console.log("High lvl of grid");
     } else {
-        alert("Please, type correct info");
-        i--;
-    }   
-    console.log(i);
-};
+        console.log(Error);
+    }
+}
+detectLevel();
 
 
+function checkSavings() {
+    if (obj.savings == true) {
+        let save = +prompt('Which sum of saving?'),
+            percent = +prompt('Which percent?');
+
+            obj.monthIncome = save / 100 / 12 * percent;
+            alert('Income per month from your deposit: ' + obj.monthIncome);
+    }
+}
+checkSavings();
 
 /*
 let i = 0;
@@ -61,18 +119,3 @@ do {
     
 } while (i < 2);
 */
-obj.moneyPerDay = obj.budget / 30;
-
-alert("Budget per one day " + obj.moneyPerDay);
-
-if (obj.moneyPerDay < 100) {
-    console.log("Minimal lvl of grid"); 
-} else if(obj.moneyPerDay > 100 && obj.moneyPerDay < 2000) {
-    console.log("Middle lvl of grid");
-} else if (obj.moneyPerDay > 2000) {
-    console.log("High lvl of grid");
-} else {
-    console.log(Error);
-}
-
-console.log(expenses);
